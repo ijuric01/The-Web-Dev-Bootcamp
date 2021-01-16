10604aad4495d46e4664d2f6e7c3339d2865553e
ijuric01 <ijuric01@fesb.hr>
2021-01-16 02:09:30 +0100

    JavaScript ToDo project

--- index.js
+++ index.js
@@ -163,4 +163,39 @@
 // }
 // console.log(total/scores.length)
 
- 
+ //TODO APP
+
+ let input=prompt("what would u like to do");
+ const todos=["collect eggs", "cleaning box"];
+
+ while(input!=="quit" && input!=="q"){
+
+    if(input==="list")
+    {
+        console.log("------")
+        for(let i=0; i<todos.length; i++)
+        {
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log("------")
+
+    }
+    else if(input==="new"){
+        const newtodo=prompt("ok, what is new to do?");
+        todos.push(newtodo);
+        console.log(`${newtodo} is added to list`);
+    }
+    else if(input==="delete"){
+        const index=parseInt(prompt("ok, enter an index to delete:"));
+        if(!Number.isNaN(index)){
+            const deleted=todos.splice(index, 1);
+            console.log(`ok, deleted stuff is: ${deleted[0]}`); //jer je deleted array
+        }
+        else{
+            console.log("unknown index!")
+        }
+    }
+    input=prompt("what would u like to do?");
+
+ }
+ console.log("ok, quit the app")
