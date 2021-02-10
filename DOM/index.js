9d0ed1290bb3d20e4e3817e4e29558696e4d6e8c
ijuric01 <ijuric01@fesb.hr>
2021-02-11 00:39:23 +0100

    DOM exercise

--- DOM/index.js
+++ DOM/index.js
@@ -32,9 +32,23 @@
 // const links=document.querySelectorAll("p a");
 
 // for (let link of links){
-//     console.log(link.href)
+//     console.log(link.href)  //printam source od svakog linka
 // // }
 
+//Select all elements that have the class of "done" and save them in a variable called doneTodos.
 // const doneTodos = document.querySelectorAll('.done');
-// const checkbox = document.querySelector('input[type="checkbox"]');
+//Select the one checkbox and save it in a variable called checkbox, use atribute
+// const checkbox = document.querySelector('input[type="checkbox"]'); //css atribute selector
+
+// document.querySelector("span").textContent="iju" //doesnt care about display
+// const span = document.querySelector("span"); //innerText 
+// span.innerText = "Disgusting";
+
+//inner css
+// const image = document.querySelector("img");
+// image.style.width = '150px';
+// image.style.borderRadius = '50%';
+
+// const container = document.querySelector('#container');
+// container.style.textAlign = 'center';
 
