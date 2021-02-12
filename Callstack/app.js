c943dc3f3f47876e9db9274a0bc21fd50a8d2f00
ijuric01 <ijuric01@fesb.hr>
2021-02-12 13:50:56 +0100

    WebAPIs and Single Threaded

---
+++ Callstack/app.js
@@ -0,0 +1,18 @@
+// const multiply = (x, y) => x * y;
+
+// const square = x => multiply(x, x);
+
+// const isRightTriangle = (a, b, c) => (
+//     square(a) + square(b) === square(c)
+// )
+// console.log("BEFORE")
+// isRightTriangle(3, 4, 5)
+
+// console.log("DONEEEE!")
+
+console.log("sending request to server!")
+setTimeout(() => {
+    console.log("here is your data from the server...")
+}, 3000)
+
+console.log("i am at he end of file")
